635d1bb4d305be26708ec279f246e303d5f5ec99
Sirisha s <sirisha563125@gmail.com>
2026-03-14 18:40:59 +0530

    Add files via upload

---
+++ java/ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        String a="Sirisha";
+        for(int i=1;i<=10;i++){
+            System.out.println(a);
+        }
+        
+    }
+    
+}
