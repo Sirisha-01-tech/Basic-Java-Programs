6b3e713265836650490723478c8a194881ac7194
Sirisha s <sirisha563125@gmail.com>
2026-03-25 17:41:17 +0530

    Add files via upload

---
+++ java/Many.java
@@ -0,0 +1,17 @@
+class Multiple{
+String name;
+int age;
+String branch;
+double cgpa;
+
+}
+public class Many{
+    public static void main(String args[]){
+        Multiple m1=new Multiple();
+        m1.name="siri";
+        m1.age=20;
+        m1.branch="CSE";
+        m1.cgpa=8.9;
+        System.out.println(m1);
+    }
+}
---
+++ java/Object1.java
@@ -0,0 +1,18 @@
+class Student3{
+    String name;
+    int age;
+    String branch;
+  // void display(){
+     //   System.out.println(name+" "+age+" "+branch);
+    }
+//}
+public class Object1{
+public static void main(String args[]){
+    Student3 s1=new Student3();
+    s1.name="siri" ;
+    s1.age=20;
+    s1.branch="CSE";
+   // s1.display();
+    System.out.println(s1.name+" "+s1.age+" "+s1.branch);
+}
+}
